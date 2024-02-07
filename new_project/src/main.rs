50aa7afe93185cd5d7307f26897fb11b18ec4882
unknown <jarpit646@gmail.com>
2024-02-07 22:02:32 +0530

    rust matching

--- new_project/src/main.rs
+++ new_project/src/main.rs
@@ -26,18 +26,35 @@
 
 
 
-fn sum(numbers: &[i32]) -> i32 {
-    let mut result = 0;
-    for number in numbers {
-        result += number;
-    }
-    result
-}
+// fn sum(numbers: &[i32]) -> i32 {
+//     let mut result = 0;
+//     for number in numbers {
+//         result += number;
+//     }
+//     result
+// }
+
+// fn main() {
+//     // There are no variadic arguments in Rust
+//     let numbers = [1, 2, 3, 4, 5];
+//     let result = sum(&numbers);
+//     println!("The sum is {}",  result);
+// }
+
+use std::io;
 
 fn main() {
-    // There are no variadic arguments in Rust
-    let numbers = [1, 2, 3, 4, 5];
-    let result = sum(&numbers);
-    println!("The sum is {}", result);
+    println!("Please enter a greeting:");
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).expect("Failed to read input");
+
+    // use of match expression to pattern match against variable "name"
+    match name.trim() {
+        "Good Morning" => println!("Good Morning"),
+        "Good Bye" => println!("Sorry to see you go."),
+        "Hello" => println!("Hi, nice to meet you!"),
+        _ => println!("I can't find a greeting, good bye."),
+        
+    }
 }
 
